01ca96a5de44eea16be015df5c97c44e6fd3ef2a
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-20 13:23:12 -0400

    get Client example working

--- lib/elixir_pub_sub_dep_example.ex
+++ lib/elixir_pub_sub_dep_example.ex
@@ -1,18 +1,32 @@
-defmodule ElixirPubSubDepExample do
-  @moduledoc """
-  Documentation for ElixirPubSubDepExample.
-  """
+defmodule Client do
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirPubSubDepExample.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(client_name) do
+    spawn(fn -> loop(client_name) end)
   end
+
+  def loop(name) do
+    receive do
+      message ->
+        IO.puts "#{name} received `#{message}`"
+        loop(name)
+    end
+  end
+  def runnn do
+    {topic1, topic2} = {:erlang, :elixir}
+    {:erlang, :elixir}
+    {:ok, _pid} = PubSub.start_link()
+    {pid1, pid2, pid3} = {
+      Client.start("John"),
+      Client.start("Nick"),
+      Client.start("Tim")
+    }
+    PubSub.subscribe(pid1, topic1)
+    PubSub.subscribe(pid3, topic2)
+    PubSub.publish(topic1, "#{topic1} is great!")
+    PubSub.publish(topic2, "#{topic2} is so cool, dude")
+    PubSub.subscribe(pid2, topic1)
+    PubSub.publish(topic1, "#{topic1} is great!")
+    PubSub.subscribe(pid2, topic2)
+  end
+
 end
