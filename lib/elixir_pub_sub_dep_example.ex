9323fb1bbd2e995d1d28f7c004d02cc8f643553f
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-21 12:58:43 -0400

    use pubsub library to setup ClientCarRadio, RadioStation and Narrator

--- lib/elixir_pub_sub_dep_example.ex
+++ lib/elixir_pub_sub_dep_example.ex
@@ -1,32 +1,81 @@
-defmodule Client do
-
-  def start(client_name) do
-    spawn(fn -> loop(client_name) end)
-  end
-
+defmodule CarRadio do
+  def start(client_name) do spawn(fn -> loop(client_name) end) end
   def loop(name) do
     receive do
-      message ->
-        IO.puts "#{name} received `#{message}`"
-        loop(name)
+      message -> IO.puts "#{name} received #{message}"; loop(name);
     end
   end
-  def runnn do
-    {topic1, topic2} = {:erlang, :elixir}
-    {:erlang, :elixir}
-    {:ok, _pid} = PubSub.start_link()
-    {pid1, pid2, pid3} = {
-      Client.start("John"),
-      Client.start("Nick"),
-      Client.start("Tim")
-    }
-    PubSub.subscribe(pid1, topic1)
-    PubSub.subscribe(pid3, topic2)
-    PubSub.publish(topic1, "#{topic1} is great!")
-    PubSub.publish(topic2, "#{topic2} is so cool, dude")
-    PubSub.subscribe(pid2, topic1)
-    PubSub.publish(topic1, "#{topic1} is great!")
-    PubSub.subscribe(pid2, topic2)
+  def default_turn_on_dumb_radio do
+    [station | _tail] = Narrator.ask_stations()
+    IO.inspect('Starting Radio station')
+    pid = CarRadio.start('')
+    PubSub.subscribe(pid, station);
+    [pid, station, '']
   end
-
+  def start_station([_pid, station, name]) do
+    IO.inspect('Starting Radio station')
+    pid = CarRadio.start(name)
+    PubSub.subscribe(pid, station);
+    Process.sleep(100);
+    [pid, station, name]
+  end
+  def stop_station([pid, prev_station, name]) do
+    Process.sleep(100);
+    PubSub.unsubscribe(pid, prev_station);
+    Process.sleep(100);
+    IO.inspect('Stopping Radio station')
+    Process.sleep(10);
+    [pid, prev_station, name]
+  end
+  def change_station([pid, prev_station, name], station) do
+    Process.sleep(100);
+    PubSub.unsubscribe(pid, prev_station);
+    IO.inspect('Changing Radio station')
+    Process.sleep(1000);
+    PubSub.subscribe(pid, station);
+    Process.sleep(100);
+    [pid, station, name]
+  end
 end
+
+defmodule RadioStation do
+  def stream(station, count) do
+    spawn fn -> infinateStation(station, count) end
+  end
+  def stream2(station, count) do
+    spawn fn ->
+      Stream.cycle(1..10) |> Enum.map(fn _chunk ->
+        PubSub.publish(station, "#{station} is great! #{count}");
+        Process.sleep(10);
+      end)
+    end
+  end
+  def infinateStation(station, count) do
+    PubSub.publish(station, "#{station} is great! #{count}");
+    Process.sleep(10);
+    infinateStation(station, count + 1)
+  end
+end
+
+defmodule Narrator do
+  # alias ElixirPubSubDepExample.CarRadio
+  def ask_stations do
+    [:fm600, :fm969TheEagle]
+  end
+  def power_on_radio_towers do
+    {:ok, _pid} = PubSub.start_link()
+    ask_stations() |> Enum.with_index |> Enum.map(fn {station,i} ->
+      RadioStation.stream(station, i*10000)
+    end)
+    # RadioStation.stream(:fm600, 0);
+    # RadioStation.stream(:fm969TheEagle, 999999)
+  end
+end
+'''
+Narrator.power_on_radio_towers
+CarRadio.start_station([nil, :fm969TheEagle, "Nick"]) |> CarRadio.change_station(:fm600)
+
+Narrator.power_on_radio_towers
+CarRadio.default_turn_on_dumb_radio
+'''
+
